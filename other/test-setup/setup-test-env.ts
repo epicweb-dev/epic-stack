2b3dac999f9e9b44282064310ce76b2000d32754
onemen <tabmix.onemen@gmail.com>
2023-05-20 20:45:38 +0300

    we also need `expect.extend(matchers)`

--- other/test-setup/setup-test-env.ts
+++ other/test-setup/setup-test-env.ts
@@ -1,11 +1,14 @@
 import './setup-env-vars.ts'
-import { afterAll, afterEach } from 'vitest'
+import { afterAll, afterEach, expect } from 'vitest'
 import { installGlobals } from '@remix-run/node'
+import { matchers } from './matchers.cjs'
 import 'dotenv/config'
 import fs from 'fs'
 import { BASE_DATABASE_PATH, DATABASE_PATH } from './paths.ts'
 import { deleteAllData } from './utils.ts'
 import { prisma } from '~/utils/db.server.ts'
+
+expect.extend(matchers)
 
 installGlobals()
 fs.copyFileSync(BASE_DATABASE_PATH, DATABASE_PATH)
