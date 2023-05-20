db7b35ccd6e38be171a196ac2e9942e426508a87
onemen <tabmix.onemen@gmail.com>
2023-05-20 19:33:33 +0300

    extend vitest expect with testing library assertions

--- other/test-setup/setup-test-env.ts
+++ other/test-setup/setup-test-env.ts
@@ -1,22 +1,11 @@
 import './setup-env-vars.ts'
-import { afterAll, afterEach, expect } from 'vitest'
+import { afterAll, afterEach } from 'vitest'
 import { installGlobals } from '@remix-run/node'
-import { matchers, type TestingLibraryMatchers } from './matchers.cjs'
 import 'dotenv/config'
 import fs from 'fs'
 import { BASE_DATABASE_PATH, DATABASE_PATH } from './paths.ts'
 import { deleteAllData } from './utils.ts'
 import { prisma } from '~/utils/db.server.ts'
-
-declare global {
-	namespace Vi {
-		interface JestAssertion<T = any>
-			extends jest.Matchers<void, T>,
-				TestingLibraryMatchers<T, void> {}
-	}
-}
-
-expect.extend(matchers)
 
 installGlobals()
 fs.copyFileSync(BASE_DATABASE_PATH, DATABASE_PATH)
