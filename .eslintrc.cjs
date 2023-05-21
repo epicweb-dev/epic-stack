5a6b8d75e7caaa5603e531027a878b9c853b0723
onemen <tabmix.onemen@gmail.com>
2023-05-21 08:33:39 +0300

    use jest-testing-library rules only for app directory tests

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,15 +3,23 @@ module.exports = {
 	extends: [
 		'@remix-run/eslint-config',
 		'@remix-run/eslint-config/node',
-		'@remix-run/eslint-config/jest-testing-library',
 		'prettier',
 	],
 	overrides: [
 		{
-			files: ['*.test.ts', '*.test.tsx', '*.test.js', '*.test.jsx'],
+			extends: ['@remix-run/eslint-config/jest-testing-library'],
+			files: ['app/**/__tests__/**/*', 'app/**/*.{spec,test}.*'],
 			rules: {
-				'testing-library/prefer-screen-queries': 'off',
-				'testing-library/no-await-sync-query': 'off',
+				'testing-library/no-await-sync-events': 'off',
+				'jest-dom/prefer-in-document': 'off',
+			},
+			// we're using vitest which has a very similar API to jest
+			// (so the linting plugins work nicely), but it means we have to explicitly
+			// set the jest version.
+			settings: {
+				jest: {
+					version: 28,
+				},
 			},
 		},
 	],
@@ -24,16 +32,6 @@ module.exports = {
 				fixStyle: 'inline-type-imports',
 			},
 		],
-		'testing-library/no-await-sync-events': 'off',
-		'jest-dom/prefer-in-document': 'off',
 		'@typescript-eslint/no-duplicate-imports': 'warn',
-	},
-	// we're using vitest which has a very similar API to jest
-	// (so the linting plugins work nicely), but it means we have to explicitly
-	// set the jest version.
-	settings: {
-		jest: {
-			version: 28,
-		},
 	},
 }
