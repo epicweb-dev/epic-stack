0c0c7d9b34671342cf94a53bbbbeaf8beb173422
onemen <tabmix.onemen@gmail.com>
2023-06-04 16:32:03 +0300

    simplify dev script

--- server/dev-server.js
+++ server/dev-server.js
@@ -4,12 +4,13 @@ if (process.env.NODE_ENV === 'production') {
 	await import('./index.js')
 } else {
 	const command =
-		'tsx watch --clear-screen=false --ignore "app/**" --ignore "build/**" --ignore "node_modules/**" ./index.js'
+		'tsx watch --clear-screen=false --ignore "app/**" --ignore "build/**" --ignore "node_modules/**" --inspect ./index.js'
 	execa(command, {
 		stdio: ['ignore', 'inherit', 'inherit'],
 		env: {
 			...process.env,
 			FORCE_COLOR: true,
+			MOCKS: true,
 		},
 		// https://github.com/sindresorhus/execa/issues/433
 		windowsHide: false,
