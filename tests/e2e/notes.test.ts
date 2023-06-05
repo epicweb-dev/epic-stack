a167dc8f3ca1262359c5cdd042d3e78039dc2557
Rajesh Gollapudi <gollapudi.rajesh@gmail.com>
2023-06-05 15:29:21 -0400

    fix issues in the previous commit

--- tests/e2e/notes.test.ts
+++ tests/e2e/notes.test.ts
@@ -10,16 +10,16 @@ test('Users can create notes', async ({ login, page }) => {
 
 	// blank form submission should result in errors
 	await page.getByRole('button', { name: /submit/i }).click()
-	let countErrors = page
-		.getByText('String must contain at least 1 character(s)')
-		.count()
-	await expect(countErrors).toEqual(2)
+	// count errors
+	await expect(
+		page.getByText('String must contain at least 1 character(s)'),
+	).toHaveCount(2)
 
 	// fill in form and submit
 	await page.getByRole('textbox', { name: /title/i }).fill(newNote.title)
-	await page.getByLabel(/^content$/i).fill(newNote.content)
+	await page.getByRole('textbox', { name: /content/i }).fill(newNote.content)
 
-	await page.getByRole('button').last().click()
+	await page.getByRole('button', { name: /submit/i }).click()
 	await expect(page).toHaveURL(new RegExp(`/users/${user.username}/notes/.*`))
 })
 
@@ -29,22 +29,26 @@ test('Users can edit notes', async ({ login, page }) => {
 
 	// create a note
 	const newNote = createNewNote()
-	await page.locator("//a[text()='+ New Note']").click()
+	await page.getByRole('link', { name: '+ New Note' }).click()
 
-	await page.locator('input[name="title"]').fill(newNote.title)
-	await page.locator('textarea[name="content"]').fill(newNote.content)
-	await page.locator('button[type="submit"]').click()
+	await page.getByRole('textbox', { name: /title/i }).fill(newNote.title)
+	await page.getByRole('textbox', { name: /content/i }).fill(newNote.content)
+	await page.getByRole('button', { name: /submit/i }).click()
 	await expect(page).toHaveURL(new RegExp(`/users/${user.username}/notes/*`))
 
 	// edit the note
-	await page.locator("//a[text()='Edit']").click()
+	await page.getByRole('link', { name: 'Edit' }).click()
 	const updatedNote = createNewNote()
-	await page.locator('input[name="title"]').fill(updatedNote.title)
-	await page.locator('textarea[name="content"]').fill(updatedNote.content)
-	await page.locator('button[type="submit"]').click()
+	await page.getByRole('textbox', { name: /title/i }).fill(updatedNote.title)
+	await page
+		.getByRole('textbox', { name: /content/i })
+		.fill(updatedNote.content)
+	await page.getByRole('button', { name: /submit/i }).click()
 
-	await expect(page).toHaveURL(new RegExp(`/users/${user.username}/notes/*`))
-	await expect(page.locator('h2')).toHaveText(updatedNote.title)
+	await expect(page).toHaveURL(new RegExp(`/users/${user.username}/notes/.*`))
+	await expect(
+		page.getByRole('heading', { name: updatedNote.title }),
+	).toBeVisible()
 })
 
 test('Users can delete notes', async ({ login, page }) => {
@@ -52,18 +56,18 @@ test('Users can delete notes', async ({ login, page }) => {
 	await page.goto(`/users/${user.username}/notes`)
 
 	const newNote = createNewNote()
-	await page.locator("//a[text()='+ New Note']").click()
+	await page.getByRole('link', { name: '+ New Note' }).click()
 
-	await page.locator('input[name="title"]').fill(newNote.title)
-	await page.locator('textarea[name="content"]').fill(newNote.content)
-	await page.locator('button[type="submit"]').click()
+	await page.getByRole('textbox', { name: /title/i }).fill(newNote.title)
+	await page.getByRole('textbox', { name: /content/i }).fill(newNote.content)
+	await page.getByRole('button', { name: /submit/i }).click()
 	await expect(page).toHaveURL(new RegExp(`/users/${user.username}/notes/*`))
-	await expect(page.locator('h2')).toHaveText(newNote.title)
+	await expect(page.getByRole('heading', { name: newNote.title })).toBeVisible()
 	// count links on page
 
 	// find links with  href prefix
 	let countBefore = await page.locator('ul>li>a').count()
-	await page.locator("//div[text()='Delete']").click()
+	await page.getByRole('button', { name: /delete/i }).click()
 	await expect(page).toHaveURL(`/users/${user.username}/notes`)
 	let countAfter = await page.locator('ul>li>a').count()
 	expect(countAfter).toEqual(countBefore - 1)
