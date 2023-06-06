e50d22e28cd938ffa23e9876d464a8c294eb81f2
onemen <tabmix.onemen@gmail.com>
2023-06-06 12:11:57 +0300

    don't copy 'dev-server' to 'server-build'

--- other/build-server.ts
+++ other/build-server.ts
@@ -11,7 +11,12 @@ const here = (...s: Array<string>) => path.join(__dirname, ...s)
 const globsafe = (s: string) => s.replace(/\\/g, '/')
 
 const allFiles = globSync(globsafe(here('../server/**/*.*')), {
-	ignore: ['**/tsconfig.json', '**/eslint*', '**/__tests__/**'],
+	ignore: [
+		'server/dev-server.js', // for development only
+		'**/tsconfig.json',
+		'**/eslint*',
+		'**/__tests__/**',
+	],
 })
 
 const entries = []
@@ -31,7 +36,7 @@ console.log('building...')
 
 esbuild
 	.build({
-		entryPoints: globSync(globsafe(here('../server/**/*.+(ts|js|tsx|jsx)'))),
+		entryPoints: entries,
 		outdir: here('../server-build'),
 		target: [`node${pkg.engines.node}`],
 		platform: 'node',
