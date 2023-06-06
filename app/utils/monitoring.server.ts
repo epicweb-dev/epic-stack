6fd14384a58c758ade511770dfb10e44b2e165b8
Stephen Cefali <scefali@sentry.io>
2023-06-06 12:50:22 -0700

    remove Sentry-Prisma

--- app/utils/monitoring.server.ts
+++ app/utils/monitoring.server.ts
@@ -1,11 +1,9 @@
-import { prisma } from './db.server.ts'
-
 import * as Sentry from '@sentry/remix'
 
+// TODO: Add Prisma integration
 export function init() {
 	Sentry.init({
 		dsn: ENV.SENTRY_DSN,
 		tracesSampleRate: 1,
-		integrations: [new Sentry.Integrations.Prisma({ client: prisma })],
 	})
 }
