930737cfd4aa5a6433b52065cf50603a9380caf4
onemen <tabmix.onemen@gmail.com>
2023-06-13 17:47:03 +0300

    update dependencies and remove unused (#168)

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,6 +7,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
 	plugins: [react(), tsconfigPaths()],
+	css: { postcss: { plugins: [] } },
 	test: {
 		include: ['./app/**/*.test.{ts,tsx}'],
 		environment: 'jsdom',
