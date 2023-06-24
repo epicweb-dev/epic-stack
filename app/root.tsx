ea56d61d3f905943f88e57ee9c1daafc71cbfd79
Alem Tuzlak <t.zlak@hotmail.com>
2023-06-24 10:28:24 +0200

    Code improvements for confetti and additional cookie headers

--- app/root.tsx
+++ app/root.tsx
@@ -153,7 +153,7 @@ function App() {
 				<Links />
 			</head>
 			<body className="flex h-full flex-col justify-between bg-background text-foreground">
-				<Confetti run={data.flash?.confetti} />
+				<Confetti confetti={data.flash?.confetti} />
 				<header className="container mx-auto py-6">
 					<nav className="flex justify-between">
 						<Link to="/">
