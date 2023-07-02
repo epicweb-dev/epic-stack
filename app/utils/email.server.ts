00dc7f623082b65edd3f8b35738bdf3abb0a5bd3
xHomu <84349818+xHomu@users.noreply.github.com>
2023-07-02 08:47:02 -0700

    resend email server

--- app/utils/email.server.ts
+++ app/utils/email.server.ts
@@ -31,7 +31,7 @@ export async function sendEmail({
 	| { html: string; text: string; react?: never }
 	| { react: ReactElement; html?: never; text?: never }
 )) {
-	const from = 'hello@epicstack.dev'
+	const from = 'hello@mud.chat'
 
 	const email = {
 		from,
