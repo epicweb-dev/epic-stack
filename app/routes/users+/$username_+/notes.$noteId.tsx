081824f7ab97c5babb2cc7c96a5c8fc73c49147c
Kent C. Dodds <me+github@kentcdodds.com>
2023-07-10 10:12:39 -0600

    move delete-note into the notes component
    Should have been this way the whole time

--- app/routes/users+/$username_+/notes.$noteId.tsx
+++ app/routes/users+/$username_+/notes.$noteId.tsx
@@ -1,13 +1,25 @@
+import { useForm } from '@conform-to/react'
+import { getFieldsetConstraint, parse } from '@conform-to/zod'
 import { json, type DataFunctionArgs } from '@remix-run/node'
-import { Link, useLoaderData } from '@remix-run/react'
+import {
+	Form,
+	Link,
+	useActionData,
+	useFormAction,
+	useLoaderData,
+	useNavigation,
+} from '@remix-run/react'
+import { formatDistanceToNow } from 'date-fns'
+import { z } from 'zod'
 import { GeneralErrorBoundary } from '~/components/error-boundary.tsx'
+import { floatingToolbarClassName } from '~/components/floating-toolbar.tsx'
+import { ErrorList } from '~/components/forms.tsx'
 import { Button } from '~/components/ui/button.tsx'
 import { Icon } from '~/components/ui/icon.tsx'
-import { DeleteNote } from '~/routes/resources+/delete-note.tsx'
-import { getUserId } from '~/utils/auth.server.ts'
+import { StatusButton } from '~/components/ui/status-button.tsx'
+import { getUserId, requireUserId } from '~/utils/auth.server.ts'
 import { prisma } from '~/utils/db.server.ts'
-import { formatDistanceToNow } from 'date-fns'
-import { floatingToolbarClassName } from '~/components/floating-toolbar.tsx'
+import { redirectWithToast } from '~/utils/flash-session.server.ts'
 
 export async function loader({ request, params }: DataFunctionArgs) {
 	const userId = await getUserId(request)
@@ -33,6 +45,54 @@ export async function loader({ request, params }: DataFunctionArgs) {
 		timeAgo,
 		dateDisplay: date.toLocaleDateString(),
 		isOwner: userId === note.ownerId,
+	})
+}
+
+const DeleteFormSchema = z.object({
+	intent: z.literal('delete-note'),
+	noteId: z.string(),
+})
+
+export async function action({ request }: DataFunctionArgs) {
+	const userId = await requireUserId(request)
+	const formData = await request.formData()
+	const submission = parse(formData, {
+		schema: DeleteFormSchema,
+		acceptMultipleErrors: () => true,
+	})
+	if (!submission.value || submission.intent !== 'submit') {
+		return json(
+			{
+				status: 'error',
+				submission,
+			} as const,
+			{ status: 400 },
+		)
+	}
+
+	const { noteId } = submission.value
+
+	const note = await prisma.note.findFirst({
+		select: { id: true, owner: { select: { username: true } } },
+		where: {
+			id: noteId,
+			ownerId: userId,
+		},
+	})
+	if (!note) {
+		submission.error.noteId = ['Note not found']
+		return json({ status: 'error', submission } as const, {
+			status: 404,
+		})
+	}
+
+	await prisma.note.delete({
+		where: { id: note.id },
+	})
+
+	return redirectWithToast(`/users/${note.owner.username}/notes`, {
+		title: 'Note deleted',
+		variant: 'destructive',
 	})
 }
 
@@ -76,6 +136,46 @@ export default function NoteRoute() {
 				</div>
 			) : null}
 		</>
+	)
+}
+
+export function DeleteNote({ id }: { id: string }) {
+	const actionData = useActionData<typeof action>()
+	const navigation = useNavigation()
+	const formAction = useFormAction()
+	const [form] = useForm({
+		id: 'delete-note',
+		lastSubmission: actionData?.submission,
+		constraint: getFieldsetConstraint(DeleteFormSchema),
+		onValidate({ formData }) {
+			return parse(formData, { schema: DeleteFormSchema })
+		},
+	})
+
+	return (
+		<Form method="post" {...form.props}>
+			<input type="hidden" name="noteId" value={id} />
+			<StatusButton
+				type="submit"
+				name="intent"
+				value="delete-note"
+				variant="destructive"
+				status={
+					navigation.state === 'submitting' &&
+					navigation.formAction === formAction &&
+					navigation.formData?.get('intent') === 'delete-note' &&
+					navigation.formMethod === 'POST'
+						? 'pending'
+						: actionData?.status ?? 'idle'
+				}
+				disabled={navigation.state !== 'idle'}
+				className="w-full max-md:aspect-square max-md:px-0"
+			>
+				<Icon name="trash" className="scale-125 max-md:scale-150 md:mr-2" />
+				<span className="max-md:hidden">Delete</span>
+			</StatusButton>
+			<ErrorList errors={form.errors} id={form.errorId} />
+		</Form>
 	)
 }
 
