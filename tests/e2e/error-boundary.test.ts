b67065ab02eb298fe7b54e5c44137679b03e5897
Vinnie <vinnie_watson@hotmail.com>
2023-07-11 05:08:57 +1200

    Add ErrorBoundary root.tsx and add 404 handling (#255)
    Co-authored-by: Kent C. Dodds <me+github@kentcdodds.com>

---
+++ tests/e2e/error-boundary.test.ts
@@ -0,0 +1,8 @@
+import { expect, test } from '../playwright-utils.ts'
+
+test('Test root error boundary caught', async ({ page }) => {
+	await page.goto('/does-not-exist')
+
+	await expect(page.getByText(/We can't find this page/i)).toBeVisible()
+	// TODO: figure out how to assert the 404 status code
+})
