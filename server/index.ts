e0c689fe001f0306964b13079d8c252fa33177aa
Edward Stone <ed@autotelic.com>
2023-07-18 16:44:29 -0700

    working PoC fastify migration

--- server/index.ts
+++ server/index.ts
@@ -1,191 +1,147 @@
 import path from 'path'
 import { fileURLToPath } from 'url'
-import express from 'express'
 import chokidar from 'chokidar'
-import compression from 'compression'
-import morgan from 'morgan'
 import address from 'address'
-import closeWithGrace from 'close-with-grace'
-import helmet from 'helmet'
+import Fastify from 'fastify'
+import { remixFastifyPlugin } from '@mcansh/remix-fastify'
+import fastifyStatic from '@fastify/static'
+import helmet from '@fastify/helmet'
 import crypto from 'crypto'
-import {
-	type RequestHandler,
-	createRequestHandler as _createRequestHandler,
-} from '@remix-run/express'
-import { wrapExpressCreateRequestHandler } from '@sentry/remix'
-import { type ServerBuild, broadcastDevReady } from '@remix-run/node'
 import getPort, { portNumbers } from 'get-port'
+import { type ServerBuild, broadcastDevReady } from '@remix-run/node'
 import chalk from 'chalk'
 
-// @ts-ignore - this file may not exist if you haven't built yet, but it will
-// definitely exist by the time the dev or prod server actually runs.
 import * as remixBuild from '../build/index.js'
 const MODE = process.env.NODE_ENV
 
-const createRequestHandler = wrapExpressCreateRequestHandler(
-	_createRequestHandler,
-)
-
 const BUILD_PATH = '../build/index.js'
+const dirname = path.dirname(fileURLToPath(import.meta.url))
 
 const build = remixBuild as unknown as ServerBuild
 let devBuild = build
 
-const app = express()
-
-const getHost = (req: { get: (key: string) => string | undefined }) =>
-	req.get('X-Forwarded-Host') ?? req.get('host') ?? ''
-
-// ensure HTTPS only (X-Forwarded-Proto comes from Fly)
-app.use((req, res, next) => {
-	const proto = req.get('X-Forwarded-Proto')
-	const host = getHost(req)
-	if (proto === 'http') {
-		res.set('X-Forwarded-Proto', 'https')
-		res.redirect(`https://${host}${req.originalUrl}`)
-		return
-	}
-	next()
+const fastify = Fastify({
+	ignoreTrailingSlash: true,
+	ignoreDuplicateSlashes: true,
 })
 
-// no ending slashes for SEO reasons
-// https://github.com/epicweb-dev/epic-stack/discussions/108
-app.use((req, res, next) => {
-	if (req.path.endsWith('/') && req.path.length > 1) {
-		const query = req.url.slice(req.path.length)
-		const safepath = req.path.slice(0, -1).replace(/\/+/g, '/')
-		res.redirect(301, safepath + query)
-	} else {
-		next()
-	}
-})
+await fastify.register(import('@fastify/compress'))
 
-app.use(compression())
+const cspNonce = crypto.randomBytes(16).toString('hex')
 
-// http://expressjs.com/en/advanced/best-practice-security.html#at-a-minimum-disable-x-powered-by-header
-app.disable('x-powered-by')
-
-// Remix fingerprints its assets so we can cache forever.
-app.use(
-	'/build',
-	express.static('public/build', { immutable: true, maxAge: '1y' }),
-)
-
-// Aggressively cache fonts for a year
-app.use(
-	'/fonts',
-	express.static('public/fonts', { immutable: true, maxAge: '1y' }),
-)
-
-// Everything else (like favicon.ico) is cached for an hour. You may want to be
-// more aggressive with this caching.
-app.use(express.static('public', { maxAge: '1h' }))
-
-morgan.token('url', (req, res) => decodeURIComponent(req.url ?? ''))
-app.use(morgan('tiny'))
-
-app.use((_, res, next) => {
-	res.locals.cspNonce = crypto.randomBytes(16).toString('hex')
-	next()
-})
-
-app.use(
-	helmet({
-		crossOriginEmbedderPolicy: false,
-		contentSecurityPolicy: {
-			// NOTE: Remove reportOnly when you're ready to enforce this CSP
-			reportOnly: true,
-			directives: {
-				'connect-src': [
-					MODE === 'development' ? 'ws:' : null,
-					process.env.SENTRY_DSN ? '*.ingest.sentry.io' : null,
-					"'self'",
-				].filter(Boolean),
-				'font-src': ["'self'"],
-				'frame-src': ["'self'"],
-				'img-src': ["'self'", 'data:'],
-				'script-src': [
-					"'strict-dynamic'",
-					"'self'",
-					// @ts-expect-error
-					(_, res) => `'nonce-${res.locals.cspNonce}'`,
-				],
-				'script-src-attr': [
-					// @ts-expect-error
-					(_, res) => `'nonce-${res.locals.cspNonce}'`,
-				],
-				'upgrade-insecure-requests': null,
-			},
+fastify.register(helmet, {
+	crossOriginEmbedderPolicy: false,
+	enableCSPNonces: true,
+	contentSecurityPolicy: {
+		// NOTE: Remove reportOnly when you're ready to enforce this CSP
+		reportOnly: true,
+		directives: {
+			'connect-src': [
+				MODE === 'development' ? 'ws:' : null,
+				process.env.SENTRY_DSN ? '*.ingest.sentry.io' : null,
+				"'self'",
+			].filter(Boolean),
+			'font-src': ["'self'"],
+			'frame-src': ["'self'"],
+			'img-src': ["'self'", 'data:'],
+			'script-src': [
+				"'strict-dynamic'",
+				"'self'",
+				// @ts-expect-error
+				(_, res) => (res.scriptNonce	= cspNonce),
+			],
+			'script-src-attr': [
+				(_, res) => `'nonce-${cspNonce}'`,
+			],
+			'upgrade-insecure-requests': null,
 		},
-	}),
-)
+	},
+})
 
-function getRequestHandler(build: ServerBuild): RequestHandler {
-	function getLoadContext(_: any, res: any) {
-		return { cspNonce: res.locals.cspNonce }
-	}
-	return createRequestHandler({ build, mode: MODE, getLoadContext })
-}
+await fastify.register(remixFastifyPlugin, {
+	build,
+	mode: MODE,
+	getLoadContext: () => ({}),
+	rootDir: dirname,
+	purgeRequireCacheInDevelopment: false,
+	unstable_earlyHints: true
+})
 
-app.all(
-	'*',
-	MODE === 'development'
-		? (...args) => getRequestHandler(devBuild)(...args)
-		: getRequestHandler(build),
-)
+fastify.register(fastifyStatic, {
+	root: path.join(dirname, '../public/build'),
+	prefix: '/build',
+	decorateReply: false
+})
+
+fastify.register(fastifyStatic, {
+	root: path.join(dirname, '../public/fonts'),
+	prefix: '/fonts',
+	decorateReply: false
+})
+
+fastify.register(fastifyStatic, {
+	root: path.join(dirname, '../public'),
+	prefix: '/public',
+	decorateReply: false
+})
 
 const desiredPort = Number(process.env.PORT || 3000)
 const portToUse = await getPort({
 	port: portNumbers(desiredPort, desiredPort + 100),
 })
 
-const server = app.listen(portToUse, () => {
-	const addy = server.address()
-	const portUsed =
-		desiredPort === portToUse
-			? desiredPort
-			: addy && typeof addy === 'object'
-			? addy.port
-			: 0
+const start = async () => {
+	try {
+		await fastify.listen({
+			port: portToUse,
+			host: '0.0.0.0', // all IPv4, see https://fastify.dev/docs/latest/Reference/Server/#listentextresolver
+		})
+		const { port: actualPort } = fastify.addresses()[0]
+		const portUsed =
+			desiredPort === portToUse
+				? desiredPort
+				: actualPort
 
-	if (portUsed !== desiredPort) {
-		console.warn(
-			chalk.yellow(
-				`⚠️  Port ${desiredPort} is not available, using ${portUsed} instead.`,
-			),
-		)
-	}
-	console.log(`🚀  We have liftoff!`)
-	const localUrl = `http://localhost:${portUsed}`
-	let lanUrl: string | null = null
-	const localIp = address.ip()
-	// Check if the address is a private ip
-	// https://en.wikipedia.org/wiki/Private_network#Private_IPv4_address_spaces
-	// https://github.com/facebook/create-react-app/blob/d960b9e38c062584ff6cfb1a70e1512509a966e7/packages/react-dev-utils/WebpackDevServerUtils.js#LL48C9-L54C10
-	if (/^10[.]|^172[.](1[6-9]|2[0-9]|3[0-1])[.]|^192[.]168[.]/.test(localIp)) {
-		lanUrl = `http://${localIp}:${portUsed}`
-	}
+		if (portUsed !== desiredPort) {
+			console.warn(
+				chalk.yellow(
+					`⚠️  Port ${desiredPort} is not available, using ${portUsed} instead.`,
+				),
+			)
+		}
 
-	console.log(
-		`
+		console.log(fastify.server.address())
+		console.log(`🚀  We have liftoff!`)
+		const localUrl = `http://localhost:${portUsed}`
+		let lanUrl: string | null = null
+		const localIp = address.ip()
+		// Check if the address is a private ip
+		// https://en.wikipedia.org/wiki/Private_network#Private_IPv4_address_spaces
+		// https://github.com/facebook/create-react-app/blob/d960b9e38c062584ff6cfb1a70e1512509a966e7/packages/react-dev-utils/WebpackDevServerUtils.js#LL48C9-L54C10
+		if (/^10[.]|^172[.](1[6-9]|2[0-9]|3[0-1])[.]|^192[.]168[.]/.test(localIp)) {
+			lanUrl = `http://${localIp}:${portUsed}`
+		}
+
+		console.log(
+			`
 ${chalk.bold('Local:')}            ${chalk.cyan(localUrl)}
 ${lanUrl ? `${chalk.bold('On Your Network:')}  ${chalk.cyan(lanUrl)}` : ''}
 ${chalk.bold('Press Ctrl+C to stop')}
 		`.trim(),
-	)
+		)
 
-	if (MODE === 'development') {
-		broadcastDevReady(build)
+		if (MODE === 'development') {
+			broadcastDevReady(build)
+		}
+
+	} catch (err) {
+		console.log(err)
+		fastify.log.error(err)
+		process.exit(1)
 	}
-})
+}
+start()
 
-closeWithGrace(async () => {
-	await new Promise((resolve, reject) => {
-		server.close(e => (e ? reject(e) : resolve('ok')))
-	})
-})
-
-// during dev, we'll keep the build module up to date with the changes
 if (MODE === 'development') {
 	async function reloadBuild() {
 		devBuild = await import(`${BUILD_PATH}?update=${Date.now()}`)
@@ -195,5 +151,6 @@ if (MODE === 'development') {
 	const dirname = path.dirname(fileURLToPath(import.meta.url))
 	const watchPath = path.join(dirname, BUILD_PATH).replace(/\\/g, '/')
 	const watcher = chokidar.watch(watchPath, { ignoreInitial: true })
+	watcher.on('all', reloadBuild)
 	watcher.on('all', reloadBuild)
 }
