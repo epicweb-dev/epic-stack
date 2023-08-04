3858fb8b37448d6d308d27a89cae94159352c172
Kent C. Dodds <me+github@kentcdodds.com>
2023-08-04 00:10:43 -0600

    remove unused dep

--- types/deps.d.ts
+++ types/deps.d.ts
@@ -1,11 +1,6 @@
 // This module should contain type definitions for modules which do not have
 // their own type definitions and are not available on DefinitelyTyped.
 
-declare module 'thirty-two' {
-	export function encode(data: string | Buffer): string
-	export function decode(data: string): Buffer
-}
-
 declare module 'tailwindcss-animate' {
 	declare const _default: {
 		handler: () => void
