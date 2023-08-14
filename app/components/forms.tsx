fe1494fa1945e8a3450b0526b0d18b8a9559c8d4
Kent C. Dodds <me+github@kentcdodds.com>
2023-08-14 12:04:56 -0600

    fix import

--- app/components/forms.tsx
+++ app/components/forms.tsx
@@ -1,9 +1,9 @@
 import { useInputEvent } from '@conform-to/react'
 import React, { useId, useRef } from 'react'
-import { Checkbox, type CheckboxProps } from './/ui/checkbox.tsx'
-import { Input } from './/ui/input.tsx'
-import { Label } from './/ui/label.tsx'
-import { Textarea } from './/ui/textarea.tsx'
+import { Checkbox, type CheckboxProps } from './ui/checkbox.tsx'
+import { Input } from './ui/input.tsx'
+import { Label } from './ui/label.tsx'
+import { Textarea } from './ui/textarea.tsx'
 
 export type ListOfErrors = Array<string | null | undefined> | null | undefined
 
