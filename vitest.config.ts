300fee7ed2eef03bf03a746cc86afc2c51e2a43c
Kent C. Dodds <me+github@kentcdodds.com>
2023-08-14 11:51:08 -0600

    remove tsconfig path stuff

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,11 +2,10 @@
 
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
 	// @ts-expect-error their types are wrong
-	plugins: [react(), tsconfigPaths()],
+	plugins: [react()],
 	css: { postcss: { plugins: [] } },
 	test: {
 		include: ['./app/**/*.test.{ts,tsx}'],
