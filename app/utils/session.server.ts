2d50d85dc7c46154671abd3115b38109d4689977
Mo Sattler <hi@mosattler.com>
2023-08-30 18:47:55 +0200

    remove unused exports of session.server.ts

--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -10,25 +10,3 @@ export const sessionStorage = createCookieSessionStorage({
 		secure: process.env.NODE_ENV === 'production',
 	},
 })
-
-const {
-	getSession,
-	commitSession: rootCommitSession,
-	destroySession,
-} = sessionStorage
-export { getSession, destroySession }
-
-// we have to do this because every time you commit the session you overwrite it
-// so we store the expiration time in the cookie and reset it every time we commit
-export async function commitSession(
-	...args: Parameters<typeof rootCommitSession>
-) {
-	const [session, options] = args
-	if (options?.expires) {
-		session.set('expires', options.expires)
-	}
-	const expires = session.get('expires')
-		? new Date(session.get('expires'))
-		: undefined
-	return rootCommitSession(session, { expires, ...options })
-}
