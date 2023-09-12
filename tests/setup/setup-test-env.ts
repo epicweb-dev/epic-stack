d0e12e552de50eebda8c4456472ef525ad98d643
Kent C. Dodds <me+github@kentcdodds.com>
2023-09-12 14:56:56 -0600

    format, lint fix, etc.

--- tests/setup/setup-test-env.ts
+++ tests/setup/setup-test-env.ts
@@ -6,7 +6,7 @@ import '#app/utils/env.server.ts'
 
 import { installGlobals } from '@remix-run/node'
 import { cleanup } from '@testing-library/react'
-import { afterEach, beforeEach, expect, vi, type SpyInstance } from 'vitest'
+import { afterEach, beforeEach, vi, type SpyInstance } from 'vitest'
 import { server } from '#tests/mocks/index.ts'
 import './custom-matchers.ts'
 
