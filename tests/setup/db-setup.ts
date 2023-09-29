cae58ba0990ae73aef429b696c685379cc151fa9
Kasper Peulen <kasperpeulen@gmail.com>
2023-09-29 14:10:00 +0200

    Create utility to efficiently clean up the entire database (#467)

--- tests/setup/db-setup.ts
+++ tests/setup/db-setup.ts
@@ -2,6 +2,7 @@ import path from 'node:path'
 import fsExtra from 'fs-extra'
 import { afterAll, afterEach, beforeAll } from 'vitest'
 import { BASE_DATABASE_PATH } from './global-setup.ts'
+import { cleanupDb } from '#tests/db-utils.ts'
 
 const databaseFile = `./tests/prisma/data.${process.env.VITEST_POOL_ID || 0}.db`
 const databasePath = path.join(process.cwd(), databaseFile)
@@ -12,13 +13,10 @@ beforeAll(async () => {
 })
 
 // we *must* use dynamic imports here so the process.env.DATABASE_URL is set
-// befor prisma is imported and initialized
+// before prisma is imported and initialized
 afterEach(async () => {
 	const { prisma } = await import('#app/utils/db.server.ts')
-	await prisma.user.deleteMany()
-	await prisma.verification.deleteMany()
-	await prisma.role.deleteMany()
-	await prisma.permission.deleteMany()
+	await cleanupDb(prisma)
 })
 
 afterAll(async () => {
