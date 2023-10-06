492f0c9e27e2222963f2a0701cf6cfd0b09b8b95
Kent C. Dodds <me+github@kentcdodds.com>
2023-10-06 15:08:53 -0600

    chore: fix lint

--- tests/setup/db-setup.ts
+++ tests/setup/db-setup.ts
@@ -1,8 +1,8 @@
 import path from 'node:path'
 import fsExtra from 'fs-extra'
 import { afterAll, afterEach, beforeAll } from 'vitest'
-import { BASE_DATABASE_PATH } from './global-setup.ts'
 import { cleanupDb } from '#tests/db-utils.ts'
+import { BASE_DATABASE_PATH } from './global-setup.ts'
 
 const databaseFile = `./tests/prisma/data.${process.env.VITEST_POOL_ID || 0}.db`
 const databasePath = path.join(process.cwd(), databaseFile)
