27d48e95b678ac0bee76ccdcab5ab5fdfb59df19
Pat Needham <patrick.h.needham@gmail.com>
2023-10-22 23:56:41 -0400

    page content show

---
+++ app/components/templates/layout/page-content-show.tsx
@@ -0,0 +1,49 @@
+import {
+	Content,
+	ContentActionbar,
+	ContentBody,
+	ContentHeader,
+	ContentImages,
+} from '#app/components/layout/index.ts'
+
+interface PageContentShowProps {
+	itemId: string
+	title: string
+	content: string
+	displayBar?: boolean
+	canDelete?: boolean
+	images: {
+		id: string
+		altText?: string | null
+	}[]
+	timestamp?: string
+}
+
+function PageContentShow({
+	itemId,
+	title,
+	content,
+	displayBar,
+	canDelete,
+	images,
+	timestamp,
+}: PageContentShowProps) {
+	return (
+		<Content variant="show">
+			<ContentHeader title={title} />
+			<div className={`${displayBar ? 'pb-24' : 'pb-12'} overflow-y-auto`}>
+				<ContentImages images={images} />
+				<ContentBody content={content} />
+			</div>
+			{displayBar ? (
+				<ContentActionbar
+					timestamp={timestamp}
+					canDelete={canDelete}
+					itemId={itemId}
+				/>
+			) : null}
+		</Content>
+	)
+}
+
+export { PageContentShow }
