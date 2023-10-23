6e2c7b16e31d08891a177902f70166d9ed1407c5
Pat Needham <patrick.h.needham@gmail.com>
2023-10-23 05:06:28 -0400

    removing prisma integration testing which was causing bugs

--- tests/setup/db-setup.ts
+++ tests/setup/db-setup.ts
@@ -12,6 +12,11 @@ beforeAll(async () => {
 	await fsExtra.copyFile(BASE_DATABASE_PATH, databasePath)
 })
 
+// 🐞 BUG: it seems when I run test:e2e:dev, the dev database is being reset
+// it seems playwright is preferred for running on dev database?
+// https://github.com/epicweb-dev/epic-stack/issues/460#issuecomment-1734425770
+// similar reading: https://tomslutsky.co/articles/common-pitfalls-when-writing-e2e-tests-with-playwright-and-prisma
+
 // we *must* use dynamic imports here so the process.env.DATABASE_URL is set
 // before prisma is imported and initialized
 afterEach(async () => {
