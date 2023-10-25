99b2e35a6941c73347be94c4ebee798e0550d8b2
Pat Needham <patrick.h.needham@gmail.com>
2023-10-25 19:03:24 -0400

    checkbox

--- app/components/forms.tsx
+++ app/components/forms.tsx
@@ -1,6 +1,6 @@
 import { useInputEvent } from '@conform-to/react'
 import React, { useId, useRef } from 'react'
-import { Checkbox, type CheckboxProps } from './ui/checkbox.tsx'
+import { Checkbox, type CheckboxProps } from '#app/components/index.ts'
 import { Input } from './ui/input.tsx'
 import { Label } from './ui/label.tsx'
 import { Textarea } from './ui/textarea.tsx'
@@ -19,7 +19,7 @@ export function ErrorList({
 	return (
 		<ul id={id} className="flex flex-col gap-1">
 			{errorsToRender.map(e => (
-				<li key={e} className="text-foreground-destructive text-[10px]">
+				<li key={e} className="text-[10px] text-foreground-destructive">
 					{e}
 				</li>
 			))}
--- app/components/ui/primitives/index.ts
+++ app/components/ui/primitives/index.ts
@@ -1,1 +1,2 @@
 export * from './button.tsx'
+export * from './checkbox.tsx'
