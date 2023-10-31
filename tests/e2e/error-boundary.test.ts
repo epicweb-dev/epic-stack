f340a58ecdbeeb24c7449137d58918c88697ab95
Sander Koopmans <sandkoop@gmail.com>
2023-10-31 19:34:26 +0100

    Apply suggestion

--- tests/e2e/error-boundary.test.ts
+++ tests/e2e/error-boundary.test.ts
@@ -1,17 +1,9 @@
-import { type Request } from '@playwright/test'
 import { expect, test } from '#tests/playwright-utils.ts'
 
 test('Test root error boundary caught', async ({ page }) => {
 	const pageUrl = '/does-not-exist'
-	await page.goto(pageUrl)
+	const res = await page.goto(pageUrl)
 
+	expect(res?.status()).toBe(404)
 	await expect(page.getByText(/We can't find this page/i)).toBeVisible()
-
-	const listener = (request: Request) => {
-		if (request.url().includes(pageUrl)) {
-			request.response().then(response => expect(response?.status()).toBe(404))
-		}
-	}
-
-	page.on('request', listener)
 })
