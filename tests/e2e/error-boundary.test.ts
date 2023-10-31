7417c50edf19cbf68803322fe676210959c9af6e
Sander Koopmans <42515367+SanderKoopmans@users.noreply.github.com>
2023-10-31 22:43:21 +0100

    Add 404 assertion to e2e error-boundary (#506)

--- tests/e2e/error-boundary.test.ts
+++ tests/e2e/error-boundary.test.ts
@@ -1,8 +1,9 @@
 import { expect, test } from '#tests/playwright-utils.ts'
 
 test('Test root error boundary caught', async ({ page }) => {
-	await page.goto('/does-not-exist')
+	const pageUrl = '/does-not-exist'
+	const res = await page.goto(pageUrl)
 
+	expect(res?.status()).toBe(404)
 	await expect(page.getByText(/We can't find this page/i)).toBeVisible()
-	// TODO: figure out how to assert the 404 status code
 })
