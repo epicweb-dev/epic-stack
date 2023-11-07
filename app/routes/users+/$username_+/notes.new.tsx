1ea39ee32b0aadc309bacbb902b2d0a64d20569e
Sami Al-Dury <git@aldury.com>
2023-11-08 00:38:40 +0100

    fix: `json` import (#514)

--- app/routes/users+/$username_+/notes.new.tsx
+++ app/routes/users+/$username_+/notes.new.tsx
@@ -1,4 +1,4 @@
-import { json } from '@remix-run/router'
+import { json } from '@remix-run/node'
 import { type DataFunctionArgs } from '@remix-run/server-runtime'
 import { requireUserId } from '#app/utils/auth.server.ts'
 import { NoteEditor, action } from './__note-editor.tsx'
