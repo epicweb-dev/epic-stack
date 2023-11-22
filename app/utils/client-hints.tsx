d312d7b3604be9f467fc7f2ffe8e7c4ee81147d2
Kent C. Dodds <me+github@kentcdodds.com>
2023-11-22 15:29:43 -0700

    use @epic-web/client-hints

--- app/utils/client-hints.tsx
+++ app/utils/client-hints.tsx
@@ -2,78 +2,23 @@
  * This file contains utilities for using client hints for user preference which
  * are needed by the server, but are only known by the browser.
  */
+import { getHintUtils } from '@epic-web/client-hints'
+import {
+	clientHint as colorSchemeHint,
+	subscribeToSchemeChange,
+} from '@epic-web/client-hints/color-scheme'
+import { clientHint as timeZoneHint } from '@epic-web/client-hints/time-zome'
 import { useRevalidator } from '@remix-run/react'
 import * as React from 'react'
 import { useRequestInfo } from './request-info.ts'
 
-const clientHints = {
-	theme: {
-		cookieName: 'CH-prefers-color-scheme',
-		getValueCode: `window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'`,
-		fallback: 'light',
-		transform(value: string) {
-			return value === 'dark' ? 'dark' : 'light'
-		},
-	},
-	timeZone: {
-		cookieName: 'CH-time-zone',
-		getValueCode: `Intl.DateTimeFormat().resolvedOptions().timeZone`,
-		fallback: 'UTC',
-	},
+const hintsUtils = getHintUtils({
+	theme: colorSchemeHint,
+	timeZone: timeZoneHint,
 	// add other hints here
-}
+})
 
-type ClientHintNames = keyof typeof clientHints
-
-function getCookieValue(cookieString: string, name: ClientHintNames) {
-	const hint = clientHints[name]
-	if (!hint) {
-		throw new Error(`Unknown client hint: ${name}`)
-	}
-	const value = cookieString
-		.split(';')
-		.map(c => c.trim())
-		.find(c => c.startsWith(hint.cookieName + '='))
-		?.split('=')[1]
-
-	return value ? decodeURIComponent(value) : null
-}
-
-/**
- *
- * @param request {Request} - optional request object (only used on server)
- * @returns an object with the client hints and their values
- */
-export function getHints(request?: Request) {
-	const cookieString =
-		typeof document !== 'undefined'
-			? document.cookie
-			: typeof request !== 'undefined'
-			? request.headers.get('Cookie') ?? ''
-			: ''
-
-	return Object.entries(clientHints).reduce(
-		(acc, [name, hint]) => {
-			const hintName = name as ClientHintNames
-			if ('transform' in hint) {
-				acc[hintName] = hint.transform(
-					getCookieValue(cookieString, hintName) ?? hint.fallback,
-				)
-			} else {
-				// @ts-expect-error - this is fine (PRs welcome though)
-				acc[hintName] = getCookieValue(cookieString, hintName) ?? hint.fallback
-			}
-			return acc
-		},
-		{} as {
-			[name in ClientHintNames]: (typeof clientHints)[name] extends {
-				transform: (value: any) => infer ReturnValue
-			}
-				? ReturnValue
-				: (typeof clientHints)[name]['fallback']
-		},
-	)
-}
+export const { getHints } = hintsUtils
 
 /**
  * @returns an object with the client hints and their values
@@ -90,51 +35,16 @@ export function useHints() {
  */
 export function ClientHintCheck({ nonce }: { nonce: string }) {
 	const { revalidate } = useRevalidator()
-	React.useEffect(() => {
-		const themeQuery = window.matchMedia('(prefers-color-scheme: dark)')
-		function handleThemeChange() {
-			document.cookie = `${clientHints.theme.cookieName}=${
-				themeQuery.matches ? 'dark' : 'light'
-			}; Max-Age=31536000; Path=/`
-			revalidate()
-		}
-		themeQuery.addEventListener('change', handleThemeChange)
-		return () => {
-			themeQuery.removeEventListener('change', handleThemeChange)
-		}
-	}, [revalidate])
+	React.useEffect(
+		() => subscribeToSchemeChange(() => revalidate()),
+		[revalidate],
+	)
 
 	return (
 		<script
 			nonce={nonce}
 			dangerouslySetInnerHTML={{
-				__html: `
-const cookies = document.cookie.split(';').map(c => c.trim()).reduce((acc, cur) => {
-	const [key, value] = cur.split('=');
-	acc[key] = value;
-	return acc;
-}, {});
-let cookieChanged = false;
-const hints = [
-${Object.values(clientHints)
-	.map(hint => {
-		const cookieName = JSON.stringify(hint.cookieName)
-		return `{ name: ${cookieName}, actual: String(${hint.getValueCode}), cookie: cookies[${cookieName}] }`
-	})
-	.join(',\n')}
-];
-for (const hint of hints) {
-	if (decodeURIComponent(hint.cookie) !== hint.actual) {
-		cookieChanged = true;
-		document.cookie = encodeURIComponent(hint.name) + '=' + encodeURIComponent(hint.actual) + '; Max-Age=31536000; path=/';
-	}
-}
-// if the cookie changed, reload the page, unless the browser doesn't support
-// cookies (in which case we would enter an infinite loop of reloads)
-if (cookieChanged && navigator.cookieEnabled) {
-	window.location.reload();
-}
-			`,
+				__html: hintsUtils.getClientHintCheckScript(),
 			}}
 		/>
 	)
