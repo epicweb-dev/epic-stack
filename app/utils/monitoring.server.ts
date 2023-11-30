53976613e43d34b0c5b74127a7f252017d0174e8
Son Nguyen <dev.lamson@gmail.com>
2023-11-29 17:52:46 -0800

    properly ignore healthcheck related transactions to sentry (#541)

--- app/utils/monitoring.server.ts
+++ app/utils/monitoring.server.ts
@@ -22,9 +22,19 @@ export function init() {
 			new Sentry.Integrations.Prisma({ client: prisma }),
 			new ProfilingIntegration(),
 		],
+		tracesSampler(samplingContext) {
+			// ignore healthcheck transactions by other services (consul, etc.)
+			if (samplingContext.request?.url?.includes('/resources/healthcheck')) {
+				return 0
+			}
+			return 1
+		},
 		beforeSendTransaction(event) {
 			// ignore all healthcheck related transactions
-			if (event.request?.headers?.['X-Healthcheck'] === 'true') return null
+			//  note that name of header here is case-sensitive
+			if (event.request?.headers?.['x-healthcheck'] === 'true') {
+				return null
+			}
 
 			return event
 		},
