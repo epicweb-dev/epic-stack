e6ff25c91bd94aec2199be9bff9041d115856d9b
Rajesh Gollapudi <gollapudi.rajesh@gmail.com>
2023-12-06 22:41:10 -0500

    Add error boundary for edit note route

--- app/routes/users+/$username_+/__note-editor.tsx
+++ app/routes/users+/$username_+/__note-editor.tsx
@@ -17,7 +17,7 @@ import {
 	type DataFunctionArgs,
 	type SerializeFrom,
 } from '@remix-run/node'
-import { Form, useActionData, useNavigation } from '@remix-run/react'
+import { Form, useActionData } from '@remix-run/react'
 import { useRef, useState } from 'react'
 import { AuthenticityTokenInput } from 'remix-utils/csrf/react'
 import { z } from 'zod'
@@ -32,7 +32,7 @@ import { Textarea } from '#app/components/ui/textarea.tsx'
 import { requireUserId } from '#app/utils/auth.server.ts'
 import { validateCSRF } from '#app/utils/csrf.server.ts'
 import { prisma } from '#app/utils/db.server.ts'
-import { cn, getNoteImgSrc } from '#app/utils/misc.tsx'
+import { cn, getNoteImgSrc, useIsPending } from '#app/utils/misc.tsx'
 
 const titleMinLength = 1
 const titleMaxLength = 100
@@ -187,9 +187,7 @@ export function NoteEditor({
 	>
 }) {
 	const actionData = useActionData<typeof action>()
-
-	const navigation = useNavigation()
-	const isPending = navigation.state !== 'idle'
+	const isPending = useIsPending()
 
 	const [form, fields] = useForm({
 		id: 'note-editor',
