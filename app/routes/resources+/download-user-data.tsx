00c44b6bddf8da3b8c6594ffaede7a7b0c0765a4
Jonathan Higger <59105254+jjhiggz@users.noreply.github.com>
2023-12-08 09:08:15 -0700

    Update download-user-data.tsx (#553)
    Fixed spelling error, it said "suing" which is a valid word, but should be "using"

--- app/routes/resources+/download-user-data.tsx
+++ app/routes/resources+/download-user-data.tsx
@@ -9,7 +9,7 @@ export async function loader({ request }: DataFunctionArgs) {
 		where: { id: userId },
 		// this is one of the *few* instances where you can use "include" because
 		// the goal is to literally get *everything*. Normally you should be
-		// explicit with "select". We're suing select for images because we don't
+		// explicit with "select". We're using select for images because we don't
 		// want to send back the entire blob of the image. We'll send a URL they can
 		// use to download it instead.
 		include: {
