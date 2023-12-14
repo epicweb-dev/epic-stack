6a7024474e7a9031e4c38d5ddc3d5073013234e3
Kent C. Dodds <me+github@kentcdodds.com>
2023-12-14 15:31:21 -0700

    update tsconfig for module resolution

--- app/routes/_seo+/sitemap[.]xml.ts
+++ app/routes/_seo+/sitemap[.]xml.ts
@@ -1,5 +1,4 @@
 import { generateSitemap } from '@nasa-gcn/remix-seo'
-// @ts-expect-error - this does work, though it's not exactly a public API
 import { routes } from '@remix-run/dev/server-build'
 import { type DataFunctionArgs } from '@remix-run/node'
 import { getDomainUrl } from '#app/utils/misc.tsx'
