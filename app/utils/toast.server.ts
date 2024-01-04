22c08d6c0913cc14e3834de9f236d1624c75fe91
Kent C. Dodds <me+github@kentcdodds.com>
2024-01-04 14:47:37 -0700

    whoops

--- app/utils/toast.server.ts
+++ app/utils/toast.server.ts
@@ -9,7 +9,7 @@ const ToastSchema = z.object({
 	description: z.string(),
 	id: z.string().default(() => cuid()),
 	title: z.string().optional(),
-	type: z.enum(['message', 'success', 'error']),
+	type: z.enum(['message', 'success', 'error']).default('message'),
 })
 
 export type Toast = z.infer<typeof ToastSchema>
