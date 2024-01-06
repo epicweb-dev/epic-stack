fc2c9a169667e117f07e2c25bb559e740299278b
Simon Vrachliotis <simon@simonswiss.com>
2024-01-06 20:13:47 +1100

    remove extra div

--- app/routes/_marketing+/index.tsx
+++ app/routes/_marketing+/index.tsx
@@ -31,45 +31,42 @@ export default function Index() {
 	return (
 		<main className="font-poppins grid h-full place-items-center">
 			<div className="grid place-items-center px-4 py-16 xl:grid-cols-2 xl:gap-24">
-				<div className="xl:order-2">
-					<div className="flex max-w-md flex-col items-center text-center xl:items-start xl:text-left">
+				<div className="flex max-w-md flex-col items-center text-center xl:order-2 xl:items-start xl:text-left">
+					<a
+						href="https://www.epicweb.dev/stack"
+						className="animate-slide-top [animation-fill-mode:backwards] xl:animate-slide-left xl:[animation-delay:0.5s] xl:[animation-fill-mode:backwards]"
+					>
+						<svg
+							className="size-20 text-foreground xl:-mt-4"
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 65 65"
+						>
+							<path
+								fill="currentColor"
+								d="M39.445 25.555 37 17.163 65 0 47.821 28l-8.376-2.445Zm-13.89 0L28 17.163 0 0l17.179 28 8.376-2.445Zm13.89 13.89L37 47.837 65 65 47.821 37l-8.376 2.445Zm-13.89 0L28 47.837 0 65l17.179-28 8.376 2.445Z"
+							></path>
+						</svg>
+					</a>
+					<h1
+						data-heading
+						className="mt-8 animate-slide-top text-4xl font-medium text-foreground [animation-fill-mode:backwards] [animation-delay:0.3s] md:text-5xl xl:mt-4 xl:animate-slide-left xl:text-6xl xl:[animation-fill-mode:backwards] xl:[animation-delay:0.8s]"
+					>
+						<a href="https://www.epicweb.dev/stack">The Epic Stack</a>
+					</h1>
+					<p
+						data-paragraph
+						className="mt-6 animate-slide-top text-xl/7 text-muted-foreground [animation-fill-mode:backwards] [animation-delay:0.8s] xl:mt-8 xl:animate-slide-left xl:text-xl/6 xl:leading-10 xl:[animation-fill-mode:backwards] xl:[animation-delay:1s]"
+					>
+						Check the{' '}
 						<a
-							href="https://www.epicweb.dev/stack"
-							className="animate-slide-top xl:animate-slide-left [animation-fill-mode:backwards] xl:[animation-delay:0.5s] xl:[animation-fill-mode:backwards]"
+							className="underline hover:no-underline"
+							href="https://github.com/epicweb-dev/epic-stack/blob/main/docs/getting-started.md"
 						>
-							<svg
-								className="size-20 text-foreground xl:-mt-4"
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 65 65"
-							>
-								<path
-									fill="currentColor"
-									d="M39.445 25.555 37 17.163 65 0 47.821 28l-8.376-2.445Zm-13.89 0L28 17.163 0 0l17.179 28 8.376-2.445Zm13.89 13.89L37 47.837 65 65 47.821 37l-8.376 2.445Zm-13.89 0L28 47.837 0 65l17.179-28 8.376 2.445Z"
-								></path>
-							</svg>
-						</a>
-
-						<h1
-							data-heading
-							className="animate-slide-top xl:animate-slide-left mt-8 text-4xl font-medium text-foreground [animation-fill-mode:backwards] [animation-delay:0.3s] md:text-5xl xl:mt-4 xl:text-6xl xl:[animation-fill-mode:backwards] xl:[animation-delay:0.8s]"
-						>
-							<a href="https://www.epicweb.dev/stack">The Epic Stack</a>
-						</h1>
-						<p
-							data-paragraph
-							className="animate-slide-top xl:animate-slide-left mt-6 text-xl/7 text-muted-foreground [animation-fill-mode:backwards] [animation-delay:0.8s] xl:mt-8 xl:text-xl/6 xl:leading-10 xl:[animation-fill-mode:backwards] xl:[animation-delay:1s]"
-						>
-							Check the{' '}
-							<a
-								className="underline hover:no-underline"
-								href="https://github.com/epicweb-dev/epic-stack/blob/main/docs/getting-started.md"
-							>
-								Getting Started guide
-							</a>{' '}
-							file for how to get your project off the ground!
-						</p>
-					</div>
+							Getting Started guide
+						</a>{' '}
+						file for how to get your project off the ground!
+					</p>
 				</div>
 				<ul className="mt-16 flex max-w-3xl flex-wrap justify-center gap-2 sm:gap-4 xl:mt-0 xl:grid xl:grid-flow-col xl:grid-cols-5 xl:grid-rows-6">
 					<TooltipProvider>
@@ -87,7 +84,7 @@ export default function Index() {
 									<TooltipTrigger asChild>
 										<a
 											href={logo.href}
-											className="size-20 sm:size-24 grid place-items-center rounded-2xl bg-violet-100 p-4 transition hover:-rotate-6 hover:bg-violet-200"
+											className="grid size-20 place-items-center rounded-2xl bg-violet-600/10 p-4 transition hover:-rotate-6 hover:bg-violet-600/15 sm:size-24 dark:grayscale dark:hover:grayscale-0"
 										>
 											<img src={logo.src} alt="" className="w-16" />
 										</a>
