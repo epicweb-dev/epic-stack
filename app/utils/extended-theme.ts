5dfff0b4b389379d0265b55b78aba534cf258993
Simon Vrachliotis <simon@simonswiss.com>
2024-01-07 13:34:32 +1100

    move marketing keyframes and animations in dedicated marketing preset

--- app/utils/extended-theme.ts
+++ app/utils/extended-theme.ts
@@ -99,24 +99,9 @@ export const extendedTheme = {
 			from: { height: 'var(--radix-accordion-content-height)' },
 			to: { height: '0' },
 		},
-		'roll-reveal': {
-			from: { transform: 'rotate(12deg) scale(0)', opacity: '0' },
-			to: { transform: 'rotate(0deg) scale(1)', opacity: '1' },
-		},
-		'slide-left': {
-			from: { transform: 'translateX(20px)', opacity: '0' },
-			to: { transform: 'translateX(0px)', opacity: '1' },
-		},
-		'slide-top': {
-			from: { transform: 'translateY(20px)', opacity: '0' },
-			to: { transform: 'translateY(0px)', opacity: '1' },
-		},
 	},
 	animation: {
 		'accordion-down': 'accordion-down 0.2s ease-out',
 		'accordion-up': 'accordion-up 0.2s ease-out',
-		'roll-reveal': 'roll-reveal 0.4s cubic-bezier(.22,1.28,.54,.99)',
-		'slide-left': 'slide-left 0.3s ease-out',
-		'slide-top': 'slide-top 0.3s ease-out',
 	},
 } satisfies Config['theme']
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import { type Config } from 'tailwindcss'
 import animatePlugin from 'tailwindcss-animate'
 import radixPlugin from 'tailwindcss-radix'
+import { marketingPreset } from './app/routes/_marketing+/tailwind-preset'
 import { extendedTheme } from './app/utils/extended-theme.ts'
 
 export default {
@@ -16,5 +17,6 @@ export default {
 		},
 		extend: extendedTheme,
 	},
+	presets: [marketingPreset],
 	plugins: [animatePlugin, radixPlugin],
 } satisfies Config
