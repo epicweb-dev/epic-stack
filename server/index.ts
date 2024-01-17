bb0dda361d82dc5a83909f098a81e4b01f9d7647
Kent C. Dodds <me+github@kentcdodds.com>
2024-01-17 11:40:12 -0700

    nothing is really "permanent"

--- server/index.ts
+++ server/index.ts
@@ -65,7 +65,7 @@ app.get('*', (req, res, next) => {
 	if (req.path.endsWith('/') && req.path.length > 1) {
 		const query = req.url.slice(req.path.length)
 		const safepath = req.path.slice(0, -1).replace(/\/+/g, '/')
-		res.redirect(301, safepath + query)
+		res.redirect(302, safepath + query)
 	} else {
 		next()
 	}
