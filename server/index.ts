a6b46acda8179ee4b1d394597a97f22187af6081
Kent C. Dodds <me+github@kentcdodds.com>
2024-01-17 10:07:23 -0700

    only remove trailing slash on GET requests
    https://github.com/epicweb-dev/epic-stack/discussions/108#discussioncomment-8157682

--- server/index.ts
+++ server/index.ts
@@ -61,7 +61,7 @@ app.use((req, res, next) => {
 
 // no ending slashes for SEO reasons
 // https://github.com/epicweb-dev/epic-stack/discussions/108
-app.use((req, res, next) => {
+app.get('*', (req, res, next) => {
 	if (req.path.endsWith('/') && req.path.length > 1) {
 		const query = req.url.slice(req.path.length)
 		const safepath = req.path.slice(0, -1).replace(/\/+/g, '/')
