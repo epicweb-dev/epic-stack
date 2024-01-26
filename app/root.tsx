d8a05e54959c91b595538824b6b8537c63c53960
Mosaad <48773133+theMosaad@users.noreply.github.com>
2024-01-26 20:53:44 +0200

    remove extra spaces

--- app/root.tsx
+++ app/root.tsx
@@ -275,10 +275,10 @@ function App() {
 function Logo() {
 	return (
 		<Link to="/" className="group grid leading-snug">
-			<span className="font-light transition group-hover:-translate-x-1	">
+			<span className="font-light transition group-hover:-translate-x-1">
 				epic
 			</span>
-			<span className="font-bold transition group-hover:translate-x-1	">
+			<span className="font-bold transition group-hover:translate-x-1">
 				notes
 			</span>
 		</Link>
