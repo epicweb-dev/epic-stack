b7190e1ea8f9a4b8393252f056fa247e3e36edc1
Kent C. Dodds <me+github@kentcdodds.com>
2024-03-06 08:21:22 -0700

    remove unused msw ping handler

--- tests/mocks/index.ts
+++ tests/mocks/index.ts
@@ -1,20 +1,9 @@
 import closeWithGrace from 'close-with-grace'
-import { passthrough, http } from 'msw'
 import { setupServer } from 'msw/node'
 import { handlers as githubHandlers } from './github.ts'
 import { handlers as resendHandlers } from './resend.ts'
 
-const miscHandlers = [
-	process.env.REMIX_DEV_ORIGIN
-		? http.post(`${process.env.REMIX_DEV_ORIGIN}ping`, passthrough)
-		: null,
-].filter(Boolean)
-
-export const server = setupServer(
-	...miscHandlers,
-	...resendHandlers,
-	...githubHandlers,
-)
+export const server = setupServer(...resendHandlers, ...githubHandlers)
 
 server.listen({ onUnhandledRequest: 'warn' })
 
