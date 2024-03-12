fd7d1541fe57050fdecba45663f2accc4eb3af8b
Frederic R <zougi.fr@gmail.com>
2024-03-12 19:22:04 +0000

    configure msw to blacklist sentry

--- tests/mocks/index.ts
+++ tests/mocks/index.ts
@@ -5,7 +5,17 @@ import { handlers as resendHandlers } from './resend.ts'
 
 export const server = setupServer(...resendHandlers, ...githubHandlers)
 
-server.listen({ onUnhandledRequest: 'warn' })
+server.listen({
+  onUnhandledRequest(request, print) {
+    // Do not print warnings on unhandled requests to Sentry.
+    if (request.url.includes('.sentry.io')) {
+      return
+    }
+
+    // Print the regular MSW unhandled request warning otherwise.
+    print.warning()
+  }
+})
 
 if (process.env.NODE_ENV !== 'test') {
 	console.info('🔶 Mock server installed')
