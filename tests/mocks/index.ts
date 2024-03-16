c423926f232dfb6d18ee67a72d1074f893ec7c9e
Frederic R <zougi.fr@gmail.com>
2024-03-16 11:32:44 +0000

    comment why using msw's onUnhandleRequests
    using request handler with passthrough requires a more permissible url catching system

--- tests/mocks/index.ts
+++ tests/mocks/index.ts
@@ -7,7 +7,10 @@ export const server = setupServer(...resendHandlers, ...githubHandlers)
 
 server.listen({
   onUnhandledRequest(request, print) {
-    // Do not print warnings on unhandled requests to Sentry.
+    // Do not print warnings on unhandled requests to https://<:userId>.ingest.us.sentry.io/api/
+    // Note: a request handler with passthrough is not suited with this type of url
+    //       until there is a more permissible url catching system
+    //       like requested at https://github.com/mswjs/msw/issues/1804
     if (request.url.includes('.sentry.io')) {
       return
     }
