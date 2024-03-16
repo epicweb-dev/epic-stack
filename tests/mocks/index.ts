bb8d6b4eb0845d059486c968d91e112cb765b642
Frederic R <zougi.fr@gmail.com>
2024-03-16 15:31:34 +0100

    configure msw to blacklist sentry (#652)
    * configure msw to blacklist sentry
    
    * comment why using msw's onUnhandleRequests
    
    using request handler with passthrough requires a more permissible url catching system

--- tests/mocks/index.ts
+++ tests/mocks/index.ts
@@ -5,7 +5,20 @@ import { handlers as resendHandlers } from './resend.ts'
 
 export const server = setupServer(...resendHandlers, ...githubHandlers)
 
-server.listen({ onUnhandledRequest: 'warn' })
+server.listen({
+  onUnhandledRequest(request, print) {
+    // Do not print warnings on unhandled requests to https://<:userId>.ingest.us.sentry.io/api/
+    // Note: a request handler with passthrough is not suited with this type of url
+    //       until there is a more permissible url catching system
+    //       like requested at https://github.com/mswjs/msw/issues/1804
+    if (request.url.includes('.sentry.io')) {
+      return
+    }
+
+    // Print the regular MSW unhandled request warning otherwise.
+    print.warning()
+  }
+})
 
 if (process.env.NODE_ENV !== 'test') {
 	console.info('🔶 Mock server installed')
