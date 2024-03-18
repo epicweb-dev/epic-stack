f10f6583bf1c854f9b20e0ad681a84e4923f6ad3
Devlin Morrow <devlin.glasman@gmail.com>
2024-03-18 21:57:12 +0100

    Remove formatting change

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -3,9 +3,9 @@ import { startTransition } from 'react'
 import { hydrateRoot } from 'react-dom/client'
 
 if (ENV.MODE === 'production' && ENV.SENTRY_DSN) {
-  import('./utils/monitoring.client.tsx').then(({ init }) => init())
+	import('./utils/monitoring.client.tsx').then(({ init }) => init())
 }
 
 startTransition(() => {
-  hydrateRoot(document, <RemixBrowser />)
+	hydrateRoot(document, <RemixBrowser />)
 })
