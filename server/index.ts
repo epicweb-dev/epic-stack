3f4db426216b881d65250ff5ad9e1b170aab428f
Pavel Mikhalkevich <paul.michelle.prod@gmail.com>
2024-03-24 20:02:46 +0500

    Re-use IS_PROD

--- server/index.ts
+++ server/index.ts
@@ -143,7 +143,7 @@ app.use(
 // rate limiting because playwright tests are very fast and we don't want to
 // have to wait for the rate limit to reset between tests.
 const maxMultiple =
-	MODE !== 'production' || process.env.PLAYWRIGHT_TEST_BASE_URL ? 10_000 : 1
+	!IS_PROD || process.env.PLAYWRIGHT_TEST_BASE_URL ? 10_000 : 1
 const rateLimitDefault = {
 	windowMs: 60 * 1000,
 	max: 1000 * maxMultiple,
