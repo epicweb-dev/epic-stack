0251f3b8dae5751bc61661a54c0581b1fda75108
Frederic R <zougi.fr@gmail.com>
2024-04-07 14:03:13 +0100

    dev-server writes to stdio without inherit
    fix https://github.com/epicweb-dev/epic-stack/issues/688

--- server/dev-server.js
+++ server/dev-server.js
@@ -1,19 +1,20 @@
-import { execa } from 'execa'
+import { spawn } from 'node:child_process'
 
 if (process.env.NODE_ENV === 'production') {
-	await import('./index.js')
+  await import('./index.js')
 } else {
-	const command =
-		'tsx watch --clear-screen=false --ignore ".cache/**" --ignore "app/**" --ignore "vite.config.ts.timestamp-*" --ignore "build/**" --ignore "node_modules/**" --inspect ./index.js'
-	execa(command, {
-		stdio: ['ignore', 'inherit', 'inherit'],
-		shell: true,
-		env: {
-			FORCE_COLOR: true,
-			MOCKS: true,
-			...process.env,
-		},
-		// https://github.com/sindresorhus/execa/issues/433
-		windowsHide: false,
-	})
+  const command =
+    'tsx watch --clear-screen=false --ignore ".cache/**" --ignore "app/**" --ignore "vite.config.ts.timestamp-*" --ignore "build/**" --ignore "node_modules/**" --inspect ./index.js'
+  const [bin, ...args] = command.split(' ')
+  const childProcess = spawn(bin, args, {
+    shell: true,
+    // do not use stdio: 'inherit'. see issue https://github.com/epicweb-dev/epic-stack/issues/688
+    env: {
+      FORCE_COLOR: true,
+      MOCKS: true,
+      ...process.env,
+    },
+  })
+  childProcess.stdout.pipe(process.stdout)
+  childProcess.stderr.pipe(process.stderr)
 }
