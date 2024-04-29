2ae7cac659b0035a17a911281b0872fdf064951c
Dennis Kraaijeveld <dfmkraaijeveld@gmail.com>
2024-04-29 17:46:15 -0500

    feat: add check to global setup

--- tests/setup/global-setup.ts
+++ tests/setup/global-setup.ts
@@ -8,8 +8,20 @@ export const BASE_DATABASE_PATH = path.join(
 )
 
 export async function setup() {
+	// Check if the database exists
 	const databaseExists = await fsExtra.pathExists(BASE_DATABASE_PATH)
-	if (databaseExists) return
+
+	if (databaseExists) {
+		const databaseLastModifiedAt = (await fsExtra.stat(BASE_DATABASE_PATH))
+			.mtime
+		const prismaSchemaLastModifiedAt = (
+			await fsExtra.stat('./prisma/schema.prisma')
+		).mtime
+
+		if (prismaSchemaLastModifiedAt < databaseLastModifiedAt) {
+			return
+		}
+	}
 
 	await execaCommand(
 		'npx prisma migrate reset --force --skip-seed --skip-generate',
