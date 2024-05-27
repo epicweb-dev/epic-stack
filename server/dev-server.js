b26b6731109a0cd0282cff97fe63b51ab14a7ca0
Kent C. Dodds <me+github@kentcdodds.com>
2024-05-27 11:35:56 -0600

    fix path to production build

--- server/dev-server.js
+++ server/dev-server.js
@@ -1,7 +1,7 @@
 import { execa } from 'execa'
 
 if (process.env.NODE_ENV === 'production') {
-	await import('./index.js')
+	await import('../server-build/index.js')
 } else {
 	const command =
 		'tsx watch --clear-screen=false --ignore ".cache/**" --ignore "app/**" --ignore "vite.config.ts.timestamp-*" --ignore "build/**" --ignore "node_modules/**" --inspect ./index.js'
