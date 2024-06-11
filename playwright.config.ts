6847347a1bddd06e0483a6c3aeff8eae141d092c
Kent C. Dodds <me+github@kentcdodds.com>
2024-06-11 12:25:40 -0600

    always reuse server if it exists
    Closes #692

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
 	webServer: {
 		command: process.env.CI ? 'npm run start:mocks' : 'npm run dev',
 		port: Number(PORT),
-		reuseExistingServer: !process.env.CI,
+		reuseExistingServer: true,
 		stdout: 'pipe',
 		stderr: 'pipe',
 		env: {
