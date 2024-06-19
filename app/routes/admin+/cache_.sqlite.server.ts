1e8c1869a0a4a6bd5cc82d48f95c3a6a80e4155a
Kent C. Dodds <me+github@kentcdodds.com>
2024-06-19 15:39:25 -0600

    consolidate cache util

--- app/routes/admin+/cache_.sqlite.server.ts
+++ app/routes/admin+/cache_.sqlite.server.ts
@@ -1,3 +1,6 @@
+import { json, redirect, type ActionFunctionArgs } from '@remix-run/node'
+import { z } from 'zod'
+import { cache } from '#app/utils/cache.server.ts'
 import {
 	getInstanceInfo,
 	getInternalInstanceDomain,
@@ -26,4 +29,30 @@ export async function updatePrimaryCacheValue({
 		},
 		body: JSON.stringify({ key, cacheValue }),
 	})
+}
+
+export async function action({ request }: ActionFunctionArgs) {
+	const { currentIsPrimary, primaryInstance } = await getInstanceInfo()
+	if (!currentIsPrimary) {
+		throw new Error(
+			`${request.url} should only be called on the primary instance (${primaryInstance})}`,
+		)
+	}
+	const token = process.env.INTERNAL_COMMAND_TOKEN
+	const isAuthorized =
+		request.headers.get('Authorization') === `Bearer ${token}`
+	if (!isAuthorized) {
+		// nah, you can't be here...
+		return redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+	}
+	const { key, cacheValue } = z
+		.object({ key: z.string(), cacheValue: z.unknown().optional() })
+		.parse(await request.json())
+	if (cacheValue === undefined) {
+		await cache.delete(key)
+	} else {
+		// @ts-expect-error - we don't reliably know the type of cacheValue
+		await cache.set(key, cacheValue)
+	}
+	return json({ success: true })
 }
