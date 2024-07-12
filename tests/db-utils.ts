ae5f3055c93f9fda0b30812dd5ca54e259dd1827
Stoica George-Ovidiu <ovidiu.stoica1094@gmail.com>
2024-07-13 02:53:37 +0300

    Fix `cleanupDb` when foreign key constraints exist (#795)
    Co-authored-by: Kent C. Dodds <me@kentcdodds.com>

--- tests/db-utils.ts
+++ tests/db-utils.ts
@@ -120,13 +120,18 @@ export async function cleanupDb(prisma: PrismaClient) {
 		{ name: string }[]
 	>`SELECT name FROM sqlite_master WHERE type='table' AND name NOT LIKE 'sqlite_%' AND name NOT LIKE '_prisma_migrations';`
 
-	await prisma.$transaction([
+	try {
 		// Disable FK constraints to avoid relation conflicts during deletion
-		prisma.$executeRawUnsafe(`PRAGMA foreign_keys = OFF`),
-		// Delete all rows from each table, preserving table structures
-		...tables.map(({ name }) =>
-			prisma.$executeRawUnsafe(`DELETE from "${name}"`),
-		),
-		prisma.$executeRawUnsafe(`PRAGMA foreign_keys = ON`),
-	])
+		await prisma.$executeRawUnsafe(`PRAGMA foreign_keys = OFF`)
+		await prisma.$transaction([
+			// Delete all rows from each table, preserving table structures
+			...tables.map(({ name }) =>
+				prisma.$executeRawUnsafe(`DELETE from "${name}"`),
+			),
+		])
+	} catch (error) {
+		console.error('Error cleaning up database:', error)
+	} finally {
+		await prisma.$executeRawUnsafe(`PRAGMA foreign_keys = ON`)
+	}
 }
