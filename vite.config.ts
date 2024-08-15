37967e98e09b77f459263aa81368b3dde2c12db8
Kent C. Dodds <me+github@kentcdodds.com>
2024-08-15 17:29:48 -0600

    move vitest config to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -76,4 +76,14 @@ export default defineConfig({
 				})
 			: null,
 	],
+	test: {
+		include: ['./app/**/*.test.{ts,tsx}'],
+		setupFiles: ['./tests/setup/setup-test-env.ts'],
+		globalSetup: ['./tests/setup/global-setup.ts'],
+		restoreMocks: true,
+		coverage: {
+			include: ['app/**/*.{ts,tsx}'],
+			all: true,
+		},
+	},
 })
