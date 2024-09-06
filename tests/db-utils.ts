6e5523f737d9626d0f46fbcb74fe1eed1661b49e
Devon Neill <35456245+devneill@users.noreply.github.com>
2024-09-06 16:51:34 +0200

    use custom db reset in `cleanupDb`

--- tests/db-utils.ts
+++ tests/db-utils.ts
@@ -115,27 +115,47 @@ export async function img({
 	}
 }
 
-export async function cleanupDb(prisma: PrismaClient, hard = true) {
-	let tables
-	if (hard) {
-		tables = await prisma.$queryRaw<
-			{ name: string }[]
-		>`SELECT name FROM sqlite_master WHERE type='table' AND name NOT LIKE 'sqlite_%' AND name NOT LIKE '_prisma_migrations';`
-	} else {
-		tables = await prisma.$queryRaw<
-			{ name: string }[]
-		>`SELECT name FROM sqlite_master WHERE type='table' AND name NOT LIKE 'sqlite_%' AND name NOT LIKE '_prisma_migrations' AND name NOT LIKE 'Role' AND name NOT LIKE 'Permission' AND name NOT LIKE '_PermissionToRole';`
-	}
+export async function cleanupDb(prisma: PrismaClient) {
+	const tables = await prisma.$queryRaw<
+		{ name: string }[]
+	>`SELECT name FROM sqlite_master WHERE type='table' AND name NOT LIKE 'sqlite_%' AND name NOT LIKE '_prisma_migrations';`
+
+	const migrationPaths = fs
+		.readdirSync('prisma/migrations')
+		.filter((dir) => dir !== 'migration_lock.toml')
+		.map((dir) => `prisma/migrations/${dir}/migration.sql`)
+
+	const migrations = migrationPaths.map((path) => {
+		// Parse the sql into individual statements
+		const sql = fs
+			.readFileSync(path)
+			.toString()
+			.split(');')
+			.map((statement) => (statement += ');'))
+
+		// Remove empty last line
+		sql.pop()
+
+		return sql
+	})
 
 	try {
 		// Disable FK constraints to avoid relation conflicts during deletion
 		await prisma.$executeRawUnsafe(`PRAGMA foreign_keys = OFF`)
 		await prisma.$transaction([
-			// Delete all rows from each table, preserving table structures
+			// Delete all tables except the ones that are excluded above
 			...tables.map(({ name }) =>
-				prisma.$executeRawUnsafe(`DELETE from "${name}"`),
+				prisma.$executeRawUnsafe(`DROP TABLE "${name}"`),
 			),
 		])
+
+		// Run the migrations sequentially
+		for (const migration of migrations) {
+			await prisma.$transaction([
+				// Run each sql statement in the migration
+				...migration.map((sql) => prisma.$executeRawUnsafe(sql)),
+			])
+		}
 	} catch (error) {
 		console.error('Error cleaning up database:', error)
 	} finally {
