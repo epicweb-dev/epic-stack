f95a03aed4c3f121388149b0fa78531fb75e064b
Sean Campbell <sean.campbell13@gmail.com>
2024-09-09 12:40:00 -0400

    fix: serverBuild access in sitemap (#845)

--- app/routes/_seo+/sitemap[.]xml.ts
+++ app/routes/_seo+/sitemap[.]xml.ts
@@ -3,8 +3,8 @@ import { type ServerBuild, type LoaderFunctionArgs } from '@remix-run/node'
 import { getDomainUrl } from '#app/utils/misc.tsx'
 
 export async function loader({ request, context }: LoaderFunctionArgs) {
-	const serverBuild = (await context.serverBuild) as ServerBuild
-	return generateSitemap(request, serverBuild.routes, {
+	const serverBuild = (await context.serverBuild) as { build: ServerBuild }
+	return generateSitemap(request, serverBuild.build.routes, {
 		siteUrl: getDomainUrl(request),
 		headers: {
 			'Cache-Control': `public, max-age=${60 * 5}`,
