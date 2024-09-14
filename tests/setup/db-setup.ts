f03db56a45588f9207689292d66c43bd4f90f1d3
Devon Neill <35456245+devneill@users.noreply.github.com>
2024-09-13 22:40:16 -0600

    Revert "Save Role seeds between integration tests (#834)"
    This reverts commit 98702a65d6f89da3ab567b0ce424d523481d7d9f.

--- tests/setup/db-setup.ts
+++ tests/setup/db-setup.ts
@@ -1,7 +1,8 @@
 import path from 'node:path'
 import fsExtra from 'fs-extra'
 import { afterAll, afterEach, beforeAll } from 'vitest'
-import { BASE_DATABASE_PATH, setup } from './global-setup.ts'
+import { cleanupDb } from '#tests/db-utils.ts'
+import { BASE_DATABASE_PATH } from './global-setup.ts'
 
 const databaseFile = `./tests/prisma/data.${process.env.VITEST_POOL_ID || 0}.db`
 const databasePath = path.join(process.cwd(), databaseFile)
@@ -11,8 +12,11 @@ beforeAll(async () => {
 	await fsExtra.copyFile(BASE_DATABASE_PATH, databasePath)
 })
 
+// we *must* use dynamic imports here so the process.env.DATABASE_URL is set
+// before prisma is imported and initialized
 afterEach(async () => {
-	await setup()
+	const { prisma } = await import('#app/utils/db.server.ts')
+	await cleanupDb(prisma)
 })
 
 afterAll(async () => {
