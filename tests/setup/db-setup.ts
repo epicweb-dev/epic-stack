7b16bdc8800efba13fa5949133b06b6b9ce725a6
Kent C. Dodds <me+github@kentcdodds.com>
2024-10-01 10:52:19 -0600

    test: copy over db instead of cleanup

--- tests/setup/db-setup.ts
+++ tests/setup/db-setup.ts
@@ -1,24 +1,19 @@
 import path from 'node:path'
 import fsExtra from 'fs-extra'
-import { afterAll, afterEach, beforeAll } from 'vitest'
-import { cleanupDb } from '#tests/db-utils.ts'
+import { afterAll, beforeEach } from 'vitest'
 import { BASE_DATABASE_PATH } from './global-setup.ts'
 
 const databaseFile = `./tests/prisma/data.${process.env.VITEST_POOL_ID || 0}.db`
 const databasePath = path.join(process.cwd(), databaseFile)
 process.env.DATABASE_URL = `file:${databasePath}`
 
-beforeAll(async () => {
+beforeEach(async () => {
 	await fsExtra.copyFile(BASE_DATABASE_PATH, databasePath)
 })
 
-// we *must* use dynamic imports here so the process.env.DATABASE_URL is set
-// before prisma is imported and initialized
-afterEach(async () => {
-	await cleanupDb()
-})
-
 afterAll(async () => {
+	// we *must* use dynamic imports here so the process.env.DATABASE_URL is set
+	// before prisma is imported and initialized
 	const { prisma } = await import('#app/utils/db.server.ts')
 	await prisma.$disconnect()
 	await fsExtra.remove(databasePath)
