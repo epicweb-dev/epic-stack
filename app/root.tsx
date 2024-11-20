4de6c13a222da6dfb133151fe8d37e24122758eb
Rajesh Gollapudi <rajesh.gollapudi@3ds.com>
2024-11-20 12:29:52 -0500

    Fix issue#806 - page jump caused by h-screen on wrapping div

--- app/root.tsx
+++ app/root.tsx
@@ -214,7 +214,7 @@ function App() {
 
 	return (
 		<>
-			<div className="flex h-screen flex-col justify-between">
+			<div className="flex min-h-screen flex-col justify-between">
 				<header className="container py-6">
 					<nav className="flex flex-wrap items-center justify-between gap-4 sm:flex-nowrap md:gap-8">
 						<Logo />
