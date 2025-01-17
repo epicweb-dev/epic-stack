3300cb2a51b91f72a00153ce382e1b3a9820e92b
Hakim Gueye <beatsjordy@gmail.com>
2025-01-17 19:00:15 +0000

    Fix: ensure resource routes return valid Response objects

--- app/routes/resources+/download-user-data.tsx
+++ app/routes/resources+/download-user-data.tsx
@@ -1,4 +1,4 @@
-import { data, type LoaderFunctionArgs } from 'react-router'
+import { type LoaderFunctionArgs } from 'react-router'
 import { requireUserId } from '#app/utils/auth.server.ts'
 import { prisma } from '#app/utils/db.server.ts'
 import { getDomainUrl } from '#app/utils/misc.tsx'
@@ -41,7 +41,7 @@ export async function loader({ request }: LoaderFunctionArgs) {
 
 	const domain = getDomainUrl(request)
 
-	return data({
+	return Response.json({
 		user: {
 			...user,
 			image: user.image
