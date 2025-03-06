cc0348efa993b8fc4c7683968f79a2c7bc7fa3e7
Aditya <kiradaditya2001@gmail.com>
2025-03-06 23:08:22 +0530

    renamed the common-password to pwnedpasswords

--- tests/mocks/index.ts
+++ tests/mocks/index.ts
@@ -1,7 +1,7 @@
 import closeWithGrace from 'close-with-grace'
 import { setupServer } from 'msw/node'
-import { pwnedPasswordApiHandlers } from './common-password.ts'
 import { handlers as githubHandlers } from './github.ts'
+import { handlers as pwnedPasswordApiHandlers } from './pwnedpasswords.ts'
 import { handlers as resendHandlers } from './resend.ts'
 import { handlers as tigrisHandlers } from './tigris.ts'
 
--- tests/mocks/pwnedpasswords.ts
+++ tests/mocks/pwnedpasswords.ts
@@ -1,6 +1,6 @@
 import { http, HttpResponse } from 'msw'
 
-export const pwnedPasswordApiHandlers = [
+export const handlers = [
 	http.get('https://api.pwnedpasswords.com/range/:prefix', () => {
 		return new HttpResponse('', { status: 200 })
 	}),
