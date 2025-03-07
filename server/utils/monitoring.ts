04890d5b9a5a4e7f9ed81f8642fe076d65eac065
Zane Hitchcox <zwhitchcox@gmail.com>
2025-03-07 09:02:35 -0500

    remove tracesSamplerate, as it is superseded by tracesSampler

--- server/utils/monitoring.ts
+++ server/utils/monitoring.ts
@@ -10,7 +10,6 @@ export function init() {
 	Sentry.init({
 		dsn: process.env.SENTRY_DSN,
 		environment: process.env.NODE_ENV,
-		tracesSampleRate: process.env.NODE_ENV === 'production' ? 1 : 0,
 		denyUrls: [
 			/\/resources\/healthcheck/,
 			// TODO: be smarter about the public assets...
