279ff8cae3847ce4fb4ec5ad25f42a7753e613de
Kent C. Dodds <me+github@kentcdodds.com>
2025-05-05 11:10:19 -0600

    fix e2e tests

--- tests/e2e/notes.test.ts
+++ tests/e2e/notes.test.ts
@@ -62,7 +62,7 @@ test('Users can delete notes', async ({ page, login }) => {
 		page.getByText('Your note has been deleted.', { exact: true }),
 	).toBeVisible()
 	await expect(page).toHaveURL(`/users/${user.username}/notes`)
-	await expect(noteLinks).toHaveCount(countBefore)
+	await expect(noteLinks).toHaveCount(countBefore - 1)
 })
 
 function createNote() {
--- tests/e2e/settings-profile.test.ts
+++ tests/e2e/settings-profile.test.ts
@@ -65,7 +65,7 @@ test('Users can update their profile photo', async ({ page, login }) => {
 	await expect(page).toHaveURL(`/settings/profile/photo`)
 
 	await page
-		.getByRole('textbox', { name: /change/i })
+		.getByRole('button', { name: /change/i })
 		.setInputFiles('./tests/fixtures/images/user/kody.png')
 
 	await page.getByRole('button', { name: /save/i }).click()
