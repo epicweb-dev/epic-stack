4e6532d08219cef41299615ad7556b205aa0b77d
Kent C. Dodds <me+github@kentcdodds.com>
2025-07-02 05:11:12 +0200

    fix litefs integration
    Closes #1017

--- app/utils/litefs.server.ts
+++ app/utils/litefs.server.ts
@@ -1,5 +1,22 @@
 // litefs-js should be used server-side only. It imports `fs` which results in Remix
 // including a big polyfill. So we put the import in a `.server.ts` file to avoid that
 // polyfill from being included. https://github.com/epicweb-dev/epic-stack/pull/331
-export * from 'litefs-js'
-export * from 'litefs-js/remix'
+export {
+	getInstanceInfo,
+	getInstanceInfoSync,
+	TXID_NUM_COOKIE_NAME,
+	waitForUpToDateTxNumber,
+	getTxNumber,
+	getTxSetCookieHeader,
+	checkCookieForTransactionalConsistency,
+	getInternalInstanceDomain,
+	getAllInstances,
+} from 'litefs-js'
+
+export {
+	ensurePrimary,
+	ensureInstance,
+	getReplayResponse,
+	handleTransactionalConsistency,
+	appendTxNumberCookie,
+} from 'litefs-js/remix'
