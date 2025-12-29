cfb8cde69eebf6f693011c5d59a0669f82e10909
Kasper Peulen <kasperpeulen@gmail.com>
2025-12-29 01:06:49 +0100

    Update heading text on users index route for testing purposes

--- app/routes/users/index.tsx
+++ app/routes/users/index.tsx
@@ -27,7 +27,7 @@ export default function UsersRoute({ loaderData }: Route.ComponentProps) {
 
 	return (
 		<div className="container mt-36 mb-48 flex flex-col items-center justify-center gap-6">
-			<h1 className="text-h1">Epic Notes Users</h1>
+			<h1 className="text-h1">Epic Notes Users Test</h1>
 			<div className="w-full max-w-[700px]">
 				<SearchBar status={loaderData.status} autoFocus autoSubmit />
 			</div>
